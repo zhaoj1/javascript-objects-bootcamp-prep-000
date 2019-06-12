368bb9db25fc13f2947bcf721e6adce82b8c2d7e
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-12 16:24:26 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,3 +5,4 @@ function updatePlaylist(playlist,artistName,songTitle){
   playlist['My Bloody Valentine']='Sometimes'
   playlist['Phil Ochs']="Here's to the State of Mississippi"
 }
+function removeFromPlaylist(playlist,artistName)
