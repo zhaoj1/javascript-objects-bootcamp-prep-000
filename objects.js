03d635486be092748b040f7cb0ec9da915095bc9
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-12 16:21:08 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,6 @@
+var playlist = {
+  Slowdive: "Alison"
+}
+function updatePlaylist(playlist,artistName,songTitle){
+  return Object.assign({},{['My Bloody Valentine']:'Sometimes'},{['Phil Ochs']:"Here's to the State of Mississippi"})
+}
